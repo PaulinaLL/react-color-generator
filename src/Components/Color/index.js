8580b6132bb453fca704fb91da3c9e0c3f401f5d
PaulinaLL <paulinalll90@wp.pl>
2020-05-07 17:05:12 +0200

    Create Color Component

---
+++ src/Components/Color/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Color(props) {
+  return (
+    <li className="newColor">
+      <div
+        className="colorHolder"
+        style={{
+          backgroundColor: `${props.color}`,
+          height: "100px",
+        }}
+      ></div>
+      <p>{props.color}</p>
+      <button onClick={() => props.remove(props.color)}>Remove</button>
+    </li>
+  );
+}
+
+export default Color;
