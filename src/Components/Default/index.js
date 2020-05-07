a6756f05a93d39f68cd3841be4732ca3d10fc41f
PaulinaLL <paulinalll90@wp.pl>
2020-05-07 17:06:08 +0200

    Create Default Component

---
+++ src/Components/Default/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Default() {
+  return (
+    <div>
+      <div className="default-pic">
+        <h4> There are no colors yet</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Default;
